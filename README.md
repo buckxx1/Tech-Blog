65480f6460f14169b47c897d8fd51570b2d0d085
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2021-05-27 12:10:29 -0400

    rever.js should be set up

---
+++ README.md
@@ -0,0 +1,16 @@
+### tech blog 
+
+this blog was made to allow users to post about whatever they want. It has the ability to allow users to create accounts.
+Users will be able to see all posts or view just their posts. they can also comment on others posts but only if they are logged in. If you idle for too long you will be loged out.
+
+### build uses 
+
+- node.js
+- express.js
+- Sequelize
+- Bycrypt
+- MVC
+- MySQL2
+- Express Session
+- Javascript
+- Handlebars.js
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const path = require("path");
+
+// controllers - handlebars - sequelize - session
+const controller = require("./controllers");
+
+const exphbs = require("express-handlebars");
+
+const sequelize = require("./config/connection");
+
+const session = require("express-session");
+const { Sequelize } = require("sequelize/types");
+
+const sequelizeStore = require("connect-session-sequelize")(session.Store)
+
+// create the session 
+const sesh = {
+    secret: "super secret secret",
+    cookie: {},
+    resave: false,
+    saveUinitialized: true,
+    store: new SequelizeStore ({
+        db: sequelize,
+    }),
+};
+
+//set port 
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+// midwear
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sesh));
+
+//controllers
+
+app.use("/", controller);
+
+//setup handlebars
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+//sync
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Server is now listening on port ${PORT}`))
+})
