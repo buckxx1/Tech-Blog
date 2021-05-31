6b1f49366d7fbb36bcef8efd48017bfe23006caa
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2021-05-31 18:13:22 -0400

    still trying to fix this

--- config/connection.js
+++ config/connection.js
@@ -12,22 +12,22 @@ const sequelize = process.env.JAWSDB_URL
         decimalNumbers: true,
       },
     });
-// if (process.env.JAWSDB_URL) {
-//   //we have jawsdb
-//   console.log("We have and will use jawsdb!");
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   //otherwise connect to local db
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME, //vars set up in .env
-//     process.env.DB_USER,
-//     process.env.DB_PW,
-//     {
-//       host: "localhost",
-//       dialect: "mysql",
-//       port: 3306,
-//     }
-//   );
-// }
+ if (process.env.JAWSDB_URL) {
+   //we have jawsdb
+   console.log("We have and will use jawsdb!");
+   sequelize = new Sequelize(process.env.JAWSDB_URL);
+ } else {
+   //otherwise connect to local db
+     sequelize = new Sequelize(
+     process.env.DB_NAME, //vars set up in .env
+     process.env.DB_USER,
+     process.env.DB_PW,
+    {
+       host: "localhost",
+       dialect: "mysql",
+       port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
